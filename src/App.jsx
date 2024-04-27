cc1910c5606e8b1cb0f20ebeee3c69447c9f4d27
Fernando Miranda <fermirandamonroy@hotmail.com>
2024-04-26 18:10:07 -0600

    funcion agregando al carrito

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,11 @@ import Header from "./components/Header";
 import { db } from "./data/db";
 function App() {
   const [data, setData] = useState(db);
+  const [car, setCar] = useState([]);
+
+  function addToCar(guitar) {
+    setCar((prevCar) => [...prevCar, guitar]);
+  }
 
   return (
     <>
@@ -14,7 +19,12 @@ function App() {
 
         <div className="row mt-5">
           {data.map((guitar) => (
-            <Guitar key={guitar.id} guitar={guitar} />
+            <Guitar
+              key={guitar.id}
+              guitar={guitar}
+              setCar={setCar}
+              addToCar={addToCar}
+            />
           ))}
         </div>
       </main>
